c1f7146d333c71746036ee675e4cd46c313b6dbf
nickolay3n <nickolay3n@gmail.com>
2020-05-01 17:08:00 +0300

    записываем проект

---
+++ construct_test/construct_test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace construct_test
+{
+    class TestConstruct {
+        static int i;
+        static  TestConstruct() { i = 1; Console.WriteLine("i = " + i);}//статический конструктор
+        public TestConstruct() { i=2; Console.WriteLine("i = " + i); }//обычный конструктор
+        public TestConstruct(int j) { i=j; Console.WriteLine("i = " + i); }// инициализатор
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestConstruct my = new TestConstruct(3);
+            Console.WriteLine("iiiiii = "+4);
+        }
+    }
+}
