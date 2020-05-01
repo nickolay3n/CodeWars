ac13627917399c2c68ab92ac6ccadc3201ec855d
nickolay3n <nickolay3n@gmail.com>
2020-05-01 14:31:55 +0300

    записываем проект

---
+++ SpinWords/SpinWords/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// Stop gninnipS My sdroW!
+/// 
+/// https://www.codewars.com/kata/5264d2b162488dc400000001
+/// 
+/// </summary>
+/// 
+
+
+namespace SpinWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string sentence = "This is another test";
+            string[] words = sentence.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 5)
+                {
+                    string reverse = "";
+                    for (int j = 0; j < (words[i].Length); j++)
+                    {
+                        reverse = reverse + words[i][words[i].Length - 1 - j];
+                    }
+                    words[i] = reverse;
+                }
+            }
+            sentence = "";
+            for (int i = 0; i < words.Length; i++)
+                sentence = sentence + " " + words[i];
+            Console.WriteLine(sentence);
+        }
+    }
+}
+
+
