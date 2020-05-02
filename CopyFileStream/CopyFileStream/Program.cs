aab856ea13124c24e95e0d6c2b7a64d0b33c8238
nickolay3n <nickolay3n@gmail.com>
2020-05-02 23:15:10 +0300

    CopyFileStream

---
+++ CopyFileStream/CopyFileStream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopyFileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
